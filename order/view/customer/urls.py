11f06d2a67addba2dba0b29119c043b90a3656a0
igoryang <yanggang1025@163.com>
2017-08-29 20:46:20 +0800

    加入 客户管理  菜单

---
+++ order/view/customer/urls.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+'''
+from django.conf.urls import include, url
+from order.view.order_cmdb import order_cmdb
+
+urlpatterns = [
+    url(r'^$', order_cmdb.zone),
+    url(r'^index$', order_cmdb.zone),
+    url(r'^zone$', order_cmdb.zone),
+    url(r'^cluster$', order_cmdb.cluster),
+    url(r'^host$', order_cmdb.host),
+    url(r'^vminstances$', order_cmdb.vminstances),
+
+]
+'''
