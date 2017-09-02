99514c25e6a125cb824597872ffe1d99d8355f74
igoryang <yanggang1025@163.com>
2017-09-02 23:05:49 +0800

    django order订单管理系统上线 订单管理 --分类管理 --订单管理 --产品管理

--- order/admin.py
+++ order/admin.py
@@ -1,11 +1,27 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
 from django.contrib import admin
-
 # Register your models here.
 from order import models
 
 # admin.site.register(models.cis,models.ci_type,models.ci_order,models.ci_product)
+class ci_tyepAdmin(admin.ModelAdmin):#表ci_type
+    list_display = ('type_id','type_name','type_alias','created_time','status') #admin后台添加字段显示
+    search_fields = ('type_id','type_name','type_alias')#添加快速查询栏
+
+class ci_productAdmin(admin.ModelAdmin):#表ci_product 用Admin的权限添加显示字段和快速查询栏
+    list_display = ('product_number','product_type','product_brand','product_model','product_alias'\
+                    ,'created_time','update_time','status')  #添加字段显示
+    search_fields = ('product_number','product_model')  #添加快速查询栏
+
+class ci_orderAdmin(admin.ModelAdmin):#表ci_order用Admin权限添加显示字段和快速查询栏
+    list_display = ('order_number','order_customer','order_type','order_brand','order_model'\
+                    ,'order_quantity','order_unitprice','order_owner','order_createdtime'\
+                    ,'order_delivertime','update_time','order_complete','order_alias','status')#添加字段显示
+
+    search_fields = ('order_number','order_brand','order_customer','order_type')#添加快速查询栏
 
 admin.site.register(models.cis)
-admin.site.register(models.ci_type)
-admin.site.register(models.ci_order)
-admin.site.register(models.ci_product)
+admin.site.register(models.ci_type,ci_tyepAdmin)
+admin.site.register(models.ci_order,ci_orderAdmin)
+admin.site.register(models.ci_product,ci_productAdmin)
