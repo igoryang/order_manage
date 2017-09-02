67a82fcde0af021b02ea625c9446dd561ec9cbb0
igoryang <yanggang1025@163.com>
2017-09-02 12:46:47 +0800

    model修改 admin后台 管理 增加中文显示

--- order/models.py
+++ order/models.py
@@ -3,10 +3,41 @@
 from time import timezone
 import django.utils.timezone as timezone
 import datetime
-
 from pytz import utc
 
 __author__ = 'igoryang'
+
+from django.contrib import admin
+from django.utils.text import capfirst
+from django.utils.datastructures import SortedDict
+
+
+def find_model_index(name):
+    count = 0
+    for model, model_admin in admin.site._registry.items():
+        if capfirst(model._meta.verbose_name_plural) == name:
+            return count
+        else:
+            count += 1
+    return count
+
+
+def index_decorator(func):
+    def inner(*args, **kwargs):
+        templateresponse = func(*args, **kwargs)
+        for app in templateresponse.context_data['app_list']:
+            app['models'].sort(key=lambda x: find_model_index(x['name']))
+        return templateresponse
+
+    return inner
+
+
+registry = SortedDict()
+registry.update(admin.site._registry)
+admin.site._registry = registry
+admin.site.index = index_decorator(admin.site.index)
+admin.site.app_index = index_decorator(admin.site.app_index)
+
 
 from django.db import models
 
@@ -53,8 +84,8 @@ class ci_order(models.Model):
     order_quantity = models.IntegerField('订单数量',max_length=11)
     order_unitprice = models.IntegerField('订单单价',max_length=1,null=True,blank=True)
     order_owner = models.CharField('业务员',max_length=50,null=True,blank=True)
-    order_createdtime = models.DateTimeField('下单时间',default=timezone)
-    order_delivertime = models.DateTimeField('交付时间',auto_now_add=True)
+    order_createdtime = models.DateTimeField('下单时间',default=timezone.now())
+    order_delivertime = models.DateTimeField('交付时间',default=timezone.now())
     update_time = models.DateTimeField('更新时间',auto_now=True)
     order_complete = models.IntegerField('完成进度',max_length=50,null=True,blank=True)
     order_alias = models.CharField('产地',max_length=50,null=True,blank=True)
