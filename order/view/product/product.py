da13ca02d6fdef68fc0c429a78636affa0bbf995
igoryang <yanggang1025@163.com>
2017-09-04 22:34:34 +0800

    product 分页功能 一页25条

--- order/view/product/product.py
+++ order/view/product/product.py
@@ -1,16 +1,28 @@
 # -*- encoding:utf-8 -*-
 
 from django.shortcuts import render, HttpResponse
-import json, chardet
+import json, chardet,os
 
 # Create your views here.
 from django.db import models
 from order.models import CiProduct   #导入 类 CiProduct
+from django.core.paginator import Paginator,EmptyPage,PageNotAnInteger  #django 分页功能
+
 
 def product(request):
-    p_list = CiProduct.objects.all()  #列出ci_product表 所有数据
-    print(p_list)
-    return render(request, 'order/product.html',{'product_list':p_list})
+    contacts_list = CiProduct.objects.all()  #列出ci_product表 所有数据
+    # print(p_list)
+    paginator = Paginator(contacts_list,25) #
+    page = request.GET.get('page')
+    try:
+        contacts = paginator.page(page)
+    except PageNotAnInteger:
+        contacts = paginator.page(1)
+    except EmptyPage:
+        contacts = paginator.page(paginator.num_pages)
+
+    return render(request, 'order/product.html',{'product_list':contacts})
+    # return render(request, 'order/product.html',{'product_list':p_list})
 
 
 # def lab(request):
